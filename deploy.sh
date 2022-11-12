7ccfe4aaa463e18c46a3749ce61c28b27300d14a
Kylen Chen[陳塏閎] <kylen.chen@kylenchen-mac13.local>
2022-11-12 23:52:11 +0800

    [功能] 修改deploy腳本

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,20 @@
-# abort on errors
-set -e
-
 # build
 npm run build
 
-git add dist -f
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
 git commit -m 'deploy'
-git subtree push --prefix dist origin gh-pages
+
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:leave3310/graduate-topic.git main:gh-pages
+
+cd -
