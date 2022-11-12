573d850123c34dfab81fcd20b389aa24d3afee82
Kylen Chen[陳塏閎] <kylen.chen@kylenchen-mac13.local>
2022-11-12 16:19:55 +0800

    [功能] 調整github page參數

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: './graduate-topic/',
 	plugins: [
 		vue(),
 		vuetify({ autoImport: true }),
